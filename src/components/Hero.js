901fac6391923399ca2fcf83cd2aa9635a803f7d
Varchasssss <137261496+Varchasssss@users.noreply.github.com>
2023-07-16 23:59:47 +0530

    Initial commit

---
+++ src/components/Hero.js
@@ -0,0 +1,16 @@
+import "./HeroStyle.css"
+
+function Hero (){
+  return(
+    <>
+    <div className="hero">
+      <img alt="HerpImg" src="https://images.unsplash.com/photo-1501785888041-af3ef285b470?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"/>
+      <div className="hero-text">
+           <
+      
+    </>
+  )
+}
+
+
+export default Hero;
